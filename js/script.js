c1a6793abbaf20d12b094a439d7a30a20755f818
Ajmal Razzaq Bhatti <ajmalrazzaq07@gmail.com>
2023-05-04 01:08:00 +0500

    mid commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
 const home = document.getElementById("home");
-const onboarding = document.getElementById("onboarding");
+const sections = {
+  home: document.getElementById("home"),
+  onboarding: document.getElementById("onboarding"),
+};
+const cardsections = {
+  image: document.getElementById("product_image"),
+  info: document.getElementById("product_info"),
+  btns: document.getElementById("product_btns"),
+};
+const cardtext = {
+  name: document.getElementById("product_name"),
+  description: document.getElementById("product_description"),
+  price: document.getElementById("product_price"),
+};
+const cardimage = {
+  bg: document.getElementById("img_bg"),
+  img: Document.getElementById("productimg"),
+};
+const cardbtns = {
+  primary: document.getElementById("primarybtn"),
+  secondary: document.getElementById("secondarybtn"),
+};
+
 function launch() {
-  home.style.display = "none";
-  onboarding.style.display = "";
+  sections.home.style.display = "none";
+  sections.onboarding.style.display = "";
 }
 function backhome() {
-  home.style.display = "";
-  onboarding.style.display = "none";
+  sections.home.style.display = "";
+  sections.onboarding.style.display = "none";
 }
-scrollReveal().reveal(".header__wrapper__brand__logo", { delay: 500 });
+function applyname() {}
