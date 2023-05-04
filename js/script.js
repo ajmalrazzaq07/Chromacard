b8cf2b412a8c5443172797b732bec317df5e1dc5
Ajmal Razzaq Bhatti <ajmalrazzaq07@gmail.com>
2023-05-04 15:41:21 +0500

    mid commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-const home = document.getElementById("home");
 const sections = {
   home: document.getElementById("home"),
   onboarding: document.getElementById("onboarding"),
@@ -15,13 +14,17 @@ const cardtext = {
 };
 const cardimage = {
   bg: document.getElementById("img_bg"),
-  img: Document.getElementById("productimg"),
+  img: document.getElementById("productimg"),
 };
 const cardbtns = {
   primary: document.getElementById("primarybtn"),
   secondary: document.getElementById("secondarybtn"),
 };
-
+const inputs = {
+  productnameinput: document.getElementById("productnameinput"),
+  productdescriptioninput: document.getElementById("productdescriptioninput"),
+  productpriceinput: document.getElementById("productpriceinput"),
+};
 function launch() {
   sections.home.style.display = "none";
   sections.onboarding.style.display = "";
@@ -30,4 +33,28 @@ function backhome() {
   sections.home.style.display = "";
   sections.onboarding.style.display = "none";
 }
-function applyname() {}
+function applyproductname() {
+  if (inputs.productnameinput.value) {
+    cardtext.name.textContent = inputs.productnameinput.value;
+  } else {
+    cardtext.name.textContent = "Name";
+  }
+}
+function applyproductdescription() {
+  if (inputs.productdescriptioninput.value) {
+    cardtext.description.textContent = inputs.productdescriptioninput.value;
+  } else {
+    cardtext.description.textContent = "Description";
+  }
+}
+function applyproductprice() {
+  if (inputs.productpriceinput.value) {
+    cardtext.price.textContent = inputs.productpriceinput.value;
+  } else {
+    cardtext.price.textContent = "$44";
+  }
+}
+var loadFile = function (event) {
+  var image = cardimage.img;
+  image.src = URL.createObjectURL(event.target.files[0]);
+};
